3e4cb1f3cf9d8a3b474dfe52b49fcb06acd2f891
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-15 16:34:56 +0100

    Implements program can be passed a number

---
+++ fizzbuzz/src/Fizzbuzz.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+class Fizzbuzz
+{
+    public function fizzbuzzer(string $number)
+    {
+        return $number;
+    }
+}
---
+++ fizzbuzz/test/FizzbuzzTest.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class FizzbuzzTest extends TestCase
+{
+    public function testProgramCanBePassed1AndReturns1()
+    {
+        $sut = new Fizzbuzz();
+        self::assertEquals('1', $sut->fizzbuzzer('1'));
+    }
+
+    public function testProgramCanBePassed2AndReturns2()
+    {
+        $sut = new Fizzbuzz();
+        self::assertEquals('2', $sut->fizzbuzzer('2'));
+    }
+}
