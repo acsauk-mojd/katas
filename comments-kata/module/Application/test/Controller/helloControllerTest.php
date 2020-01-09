eb95f161895a1a55d329e5ac1fd602be4ddb7003
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-01-09 15:19:16 +0000

    assert text element

--- comments-kata/module/Application/test/Controller/helloControllerTest.php
+++ comments-kata/module/Application/test/Controller/helloControllerTest.php
@@ -3,12 +3,38 @@
 namespace ApplicationTest\Controller;
 
 use Application\Controller\HelloController;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Application\Model\HelloTable;
+use Zend\ServiceManager\ServiceManager;
 
 class HelloControllerTest extends AbstractHttpControllerTestCase
 {
-    protected $traceError = false;
+    //protected $traceError = false;
+    protected $helloTable;
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(HelloTable::class, $this->mockHelloTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockHelloTable()
+    {
+        $this->helloTable = $this->prophesize(HelloTable::class);
+        return $this->helloTable;
+    }
 
     public function setUp() : void
     {
@@ -19,28 +45,33 @@ class HelloControllerTest extends AbstractHttpControllerTestCase
         $configOverrides = [];
 
         $this->setApplicationConfig(ArrayUtils::merge(
-        // Grabbing the full application configuration:
             include __DIR__ . '/../../../../config/application.config.php',
             $configOverrides
         ));
+
         parent::setUp();
 
-        $services = $this->getApplicationServiceLocator();
-        $config = $services->get('config');
-        unset($config['db']);
-        $services->setAllowOverride(true);
-        $services->setService('config', $config);
-        $services->setAllowOverride(false);
+        $this->configureServiceManager($this->getApplicationServiceLocator());
     }
 
     public function testIndexActionCanBeAccessed()
     {
+        $this->helloTable->fetchAll()->willReturn([]);
+
         $this->dispatch('/hello');
         $this->assertResponseStatusCode(200);
-        $this->assertModuleName('Application');
-        $this->assertControllerName(HelloController::class);
-        $this->assertControllerClass('HelloController');
-        $this->assertMatchedRouteName('world');
+        $this->assertQueryContentContains('h1','Greetings!');
+    }
+
+    /** @test */
+    public function blogPostHasComments()
+    {
+        $this->dispatch('/blog/posts/my_post');
+
+        $this->assertControllerName('blog');
+        $this->assertResponseStatusCode(200);
+        $this->assertQuery('section.blog-post>.comments>.comment');
+
     }
 
 }
