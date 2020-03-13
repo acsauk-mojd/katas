278fac2ec1923d3b06676c1406da3dbde6722f09
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2020-03-13 16:08:24 +0000

    passing test for item name

--- shopping-cart-kata/Checkout/Item.php
+++ shopping-cart-kata/Checkout/Item.php
@@ -6,5 +6,17 @@ namespace Checkout;
 
 class Item
 {
+    private $name;
 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
--- shopping-cart-kata/tests/checkoutTest.php
+++ shopping-cart-kata/tests/checkoutTest.php
@@ -14,15 +14,29 @@ class checkoutTest extends TestCase
     }
 
     /** @test */
-    public function addTwoItemsBasket() {
+    public function addOneItemBasket() {
         $basket = new Basket();
-        $items = 'Apples';
 
+        $items = 'Grapes';
         $item = new Item();
         $item->setName($items);
         $basket->addItem($item);
 
         self::assertEquals($items, $item->getName());
+    }
 
+    /** @test */
+    public function twoItemsBasket() {
+        $basket = new Basket();
+
+        $items = ['Apples', 'Grapes'];
+
+        Foreach ($items as $itemName) {
+            $item = new Item();
+            $item->setName($itemName);
+            $basket->addItem($item);
+        }
+
+        self::assertCount(2, $basket->getItems());
     }
 }
