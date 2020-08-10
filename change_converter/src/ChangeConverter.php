0b5dab276da1be2142c940d8c3ef4c6c86f74c4f
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2020-08-10 17:37:13 +0100

    refactored but most tests still broken. Fun for next person :)

--- change_converter/src/ChangeConverter.php
+++ change_converter/src/ChangeConverter.php
@@ -4,41 +4,54 @@ namespace App;
 
 class ChangeConverter
 {
-    //Array $notesAndCoins;
+    private $results = [];
+    private array $notesAndCoins = [50.00, 20.00, 10.00, 5.00. 1.00, 0.50];
+
+    public function convertDivisor(Float $change, Float $divisor)
+    {
+        while ($change / $divisor >= $divisor){
+            array_push($this->results, "£".strval($divisor));
+            $change -= $divisor;
+        }
+    }
+
     public function convert(Float $change)
     {
-        $results = [];
-
-        while ($change / 50.00 >= 1){
-            array_push($results, "£50");
-            $change -= 50.00;
+        foreach($this->notesAndCoins as $divisor);
+        {
+            $this->convertDivisor($change, $divisor);
         }
 
-        while ($change / 20.00 >= 1){
-            array_push($results, "£20");
-            $change -= 20.00;
-        }
+//        while ($change / 50.00 >= 1){
+//            array_push($this->results, "£50");
+//            $change -= 50.00;
+//        }
+//
+//        while ($change / 20.00 >= 1){
+//            array_push($this->results, "£20");
+//            $change -= 20.00;
+//        }
+//
+//        while ($change / 10.00 >= 1){
+//            array_push($this->results, "£10");
+//            $change -= 10.00;
+//        }
+//
+//        while ($change / 5.00 >= 1){
+//            array_push($this->results, "£5");
+//            $change -= 5.00;
+//        }
 
-        while ($change / 10.00 >= 1){
-            array_push($results, "£10");
-            $change -= 10.00;
-        }
+//        while ($change / 1.00 >= 1){
+//            array_push($this->results, "£1");
+//            $change -= 1.00;
+//        }
+//
+//        while ($change / 0.50 >= 1){
+//            array_push($this->results, "50p");
+//            $change -= 0.50;
+//        }
 
-        while ($change / 5.00 >= 1){
-            array_push($results, "£5");
-            $change -= 5.00;
-        }
-
-        while ($change / 1.00 >= 1){
-            array_push($results, "£1");
-            $change -= 1.00;
-        }
-
-        while ($change / 0.50 >= 1){
-            array_push($results, "50p");
-            $change -= 0.50;
-        }
-
-        return $results;
+        return $this->results;
     }
 }
