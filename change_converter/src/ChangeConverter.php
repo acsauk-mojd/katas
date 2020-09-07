9f3613a30276f241d2fb6d565ffc4beb943158a7
GemTay <gemma.taylor@digital.justice.gov.uk>
2020-09-07 16:52:07 +0100

    accepts 20p and 10p

--- change_converter/src/ChangeConverter.php
+++ change_converter/src/ChangeConverter.php
@@ -2,26 +2,23 @@
 
 namespace App;
 
+use phpDocumentor\Reflection\Types\Integer;
+
 class ChangeConverter
 {
     private $results = [];
-    private array $notesAndCoins = [50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20];
+    private array $notesAndCoins = [50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10];
 
     public function convert(Float $change)
     {
         foreach($this->notesAndCoins as $divisor)
         {
-            var_dump($change / $divisor);
-            while (($change / $divisor) >= 1 and $change !== 0.0){
-               // var_dump($change / $divisor);
-                if ($divisor <= 1 ) {
-                    var_dump($divisor);
-                   // die;
-                    $test = $divisor * 100;
-                    array_push($this->results, strval($test) . "p");
+            while (($change / $divisor) >= 1 && $change > 0){
+                var_dump("Inside while loop");
+                if ($divisor < 1) {
+                    $pence = $divisor * 100;
+                    array_push($this->results, strval($pence) . "p");
                 } else {
-                    var_dump($divisor);
-                   // die;
                     array_push($this->results, "£".strval($divisor));
                 }
                 $change -= $divisor;
--- change_converter/tests/ChangeConverterTest.php
+++ change_converter/tests/ChangeConverterTest.php
@@ -8,26 +8,26 @@ use function PHPUnit\Framework\assertEquals;
 
 class ChangeConverterTest extends TestCase
 {
-    public function testItsChangeConverterClass()
-    {
-        $test = new ChangeConverter();
-        $this->assertInstanceOf(ChangeConverter::class, $test);
-    }
-
-    public function testInput()
-    {
-        $test = new ChangeConverter();
-        $results = $test->convert(10.00);
-        $this->assertIsArray($results);
-        assertEquals(["£10"], $results);
-    }
-
-    public function testInputNotFloat()
-    {
-        $test = new ChangeConverter();
-        $this->expectException(\TypeError::class);
-        $results = $test->convert("string");
-    }
+//    public function testItsChangeConverterClass()
+//    {
+//        $test = new ChangeConverter();
+//        $this->assertInstanceOf(ChangeConverter::class, $test);
+//    }
+//
+//    public function testInput()
+//    {
+//        $test = new ChangeConverter();
+//        $results = $test->convert(10.00);
+//        $this->assertIsArray($results);
+//        assertEquals(["£10"], $results);
+//    }
+//
+//    public function testInputNotFloat()
+//    {
+//        $test = new ChangeConverter();
+//        $this->expectException(\TypeError::class);
+//        $results = $test->convert("string");
+//    }
     /**
      * @dataProvider changeProvider
      */
@@ -43,7 +43,8 @@ class ChangeConverterTest extends TestCase
             '£25 is converted to 2x £10 and 1x £5' => [["£20", "£5"], 25.00],
             '£85 is converted to 1x £50, 1x £20, 1x £10 and 1x £5' => [["£50", "£20", "£10", "£5"], 85.00],
             '£1.50 is converted to 1x £1 and 1x 50p' => [["£1", "50p"], 1.50],
-            '£1.70 is converted to 1x £1 and 1x 50p and 1x 20p' => [["£1", "50p", "20p"], 1.70],
+            '£1.80 is converted to 1x £1 and 1x 50p 1x 20p and 1x 10p' => [["£1", "50p", "20p", "10p"], 1.80],
+            '£1.88 is converted to 1x £1 and 1x 50p 1x 20p 1x 10p 1x 5p 1x 2p and 1x 1p' => [["£1", "50p", "20p", "10p", "5p", "2p", "1p"], 1.88],
         ];
     }
 
